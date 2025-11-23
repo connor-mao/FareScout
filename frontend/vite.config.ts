3b67b66252fb771b38f185b8d0d14bb17d407493
Connor Mao <cemao2012@gmail.com>
2025-11-23 12:51:34 -0800

    added flask cors

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: path.resolve(__dirname),
+  server: {
+    fs: {
+      allow: [
+        path.resolve(__dirname),
+        path.resolve(__dirname, 'node_modules/leaflet/dist/images')
+      ]
+    }
+  }
 })
